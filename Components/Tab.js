230e74e5d575bca235210cfe45e691c13b34e6f0
Pratik-Hulawale <83464018+Pratik-Hulawale@users.noreply.github.com>
2023-03-20 14:44:15 +0530

    2 files modified, 1 added

---
+++ Components/Tab.js
@@ -0,0 +1,11 @@
+import React from 'react';
+function Tab(props) {
+  return (
+    <>
+      <h1>Name:{props.name}</h1>
+      <h4>Email:{props.email}</h4>
+    </>
+  );
+}
+
+export default Tab;
--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from "react";
-import "./style.css";
-
+import React from 'react';
+import './style.css';
+import Tab from '../Components/Tab.js';
+import Data from '../Data.json';
 export default function App() {
   return (
     <div>
-      
+      {Data.map((e, i) => {
+        return <Tab name={e.name + ' ' + e.last_name} email={e.email} />;
+      })}
     </div>
   );
 }
